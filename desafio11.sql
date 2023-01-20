da60410f3f45b799ed94a7220e49c8667d4d2491
willianAD <willian.afonso013@gmail.com>
2023-01-20 19:16:51 -0300

    Requisitos 10,11,12,14 e 15

--- desafio11.sql
+++ desafio11.sql
@@ -1,2 +1,2 @@
 SELECT notes FROM purchase_orders
-WHERE notes BETWEEN 30 AND 39 ;
+WHERE notes LIKE '%3%';
--- desafio15.sql
+++ desafio15.sql
@@ -1,1 +1,1 @@
-SELECT hours(submitted_date) AS submitted_hour FROM purchase_orders;
+SELECT HOUR(submitted_date) AS submitted_hour FROM purchase_orders;
